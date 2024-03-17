62fa8117c8ed773a45119a47b2ba2d0536e04fdb
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-03-17 20:53:04 +0530

    feat: Added video carousel with that Highlights section is done

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -38,8 +39,7 @@ const Highlights = () => {
           </div>
         </div>
 
-      
-
+        <VideoCarousel />
       </div>
     </section>
   );
